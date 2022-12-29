d0baed4c8103279cf4c23ebf2eb299800ce810e2
bikash-dev <bikash.dev.bk@gmail.com>
2022-12-29 16:45:15 +0600

    basic node mongo setup

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const e = require("express");
-// require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,23 +10,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nfyjflh.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nfyjflh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // async function run() {
 //   try {
-//     const categoryCollection = client.db("gearUP").collection("categories");
+//     // const categoryCollection = client.db("gearUP").collection("categories");
 
-//     app.get("/users", async (req, res) => {
-//       const query = {};
-//       const cursor = userCollection.find(query);
-//       const users = await cursor.toArray();
-//       res.send(users);
-//     });
+//     // app.get("/users", async (req, res) => {
+//     //   const query = {};
+//     //   const cursor = userCollection.find(query);
+//     //   const users = await cursor.toArray();
+//     //   res.send(users);
+//     // });
     
 //   } finally {
 //   }
