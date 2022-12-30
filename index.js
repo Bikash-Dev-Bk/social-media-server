af721263f26a536b479a72e4b79b12b63ff86248
bikash-dev <bikash.dev.bk@gmail.com>
2022-12-30 23:14:20 +0600

    posts get and post

--- index.js
+++ index.js
@@ -21,12 +21,27 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const aboutMeCollection = client.db("InstaTown").collection("AboutMe");
+    const postCollection = client.db("InstaTown").collection("Posts");
 
     app.get("/about", async (req, res) => {
       const query = {};
       const cursor = aboutMeCollection.find(query);
       const about = await cursor.toArray();
       res.send(about);
+    });
+
+    app.get("/posts", async (req, res) => {
+      const query = {};
+      const cursor = postCollection.find(query);
+      const post = await cursor.toArray();
+      res.send(post);
+    });
+
+    app.post("/posts", async (req, res) => {
+      const post = req.body;
+      console.log(post);
+      const result = await postCollection.insertOne(post);
+      res.send(result);
     });
 
     // app.get('/about/update/:id', async (req, res) => {
